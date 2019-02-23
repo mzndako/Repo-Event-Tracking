181978fdd9e01ac6285e6601054ec62861ce7b7e
HackerRank <hackerrank@example.com>
2019-02-23 22:22:58 +0000

    Saved at Sat Feb 23 2019 23:22:57 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -4,7 +4,18 @@ const db = new sqlite.Database('database/db.sqlite')
 const find = (tableName, array)=>{
   
 }
-const test 
+const test = async function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+ 
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+ 
+  const mz = await db.each("SELECT rowid AS id, info FROM lorem");
+}
+
 db.serialize();
 
 module.exports = db
