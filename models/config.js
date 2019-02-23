d1c482c2957fd928b81e2004708f2c771e90cd08
HackerRank <hackerrank@example.com>
2019-02-23 23:29:28 +0000

    Saved at Sun Feb 24 2019 00:29:27 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -4,7 +4,7 @@ const db = new sqlite.Database('database/db.sqlite')
 const find = (tableName, where, {join, join_colum})=>{
   let sql = `SELECT * FROM ${tableName}`
   if(where){
-    sql += 
+    sql += w
   }
   return db.all(sql, (err, result) => {
     if(err){
