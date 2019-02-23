72a03a359d76589401a4b3d894b71f6105abbc3e
HackerRank <hackerrank@example.com>
2019-02-23 23:53:21 +0000

    Saved at Sun Feb 24 2019 00:53:19 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -1,7 +1,7 @@
 const sqlite = require('sqlite3').verbose()
 const db = new sqlite.Database('database/db.sqlite')
 
-const find = (tableName, where, options)=>{
+const find = (tableName, whereC, options)=>{
   let whereBind = []
   let sql = `SELECT * FROM ${tableName}`
   if(where && where[0]){
