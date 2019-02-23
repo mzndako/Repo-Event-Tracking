f959b68997c0b4139bdcef2313402a1b0d8b51d3
HackerRank <hackerrank@example.com>
2019-02-23 23:04:00 +0000

    Saved at Sun Feb 24 2019 00:03:59 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -2,7 +2,7 @@ const sqlite = require('sqlite3').verbose()
 const db = new sqlite.Database('database/db.sqlite')
 
 const find = (tableName, array)=>{
-  db.all(`SELECT   ${tableName} from `)
+  db.all(`SELECT *  ${tableName} from `)
 }
 const test = async function() {
   //db.run("CREATE TABLE lorem (info TEXT)");
