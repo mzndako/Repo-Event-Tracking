00dfe3a2b0a493f6a9194de9e24ffc3f5b735fce
HackerRank <hackerrank@example.com>
2019-02-23 23:24:58 +0000

    Saved at Sun Feb 24 2019 00:24:57 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -1,7 +1,7 @@
 const sqlite = require('sqlite3').verbose()
 const db = new sqlite.Database('database/db.sqlite')
 
-const find = (tableName, array, {popu})=>{
+const find = (tableName, array, {p})=>{
   
   db.all(`SELECT * ${tableName} from `)
 }
