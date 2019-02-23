c1a3e32a0cc0c0155b06722f2248ff6d4335c859
HackerRank <hackerrank@example.com>
2019-02-23 22:22:52 +0000

    Saved at Sat Feb 23 2019 23:22:51 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -5,16 +5,6 @@ const find = (tableName, array)=>{
   
 }
 
-db.serialize(async function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
- 
-  const mz = await db.each("SELECT rowid AS id, info FROM lorem");
-});
+db.serialize();
 
 module.exports = db
