2fedcc5aa6a50b1c008210f192812d9cc744a4a3
HackerRank <hackerrank@example.com>
2019-02-23 22:22:14 +0000

    Saved at Sat Feb 23 2019 23:22:13 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -14,7 +14,7 @@ db.serialize(function() {
   }
   stmt.finalize();
  
-  db.each("SELECT rowid AS id, info FROM lorem", );
+  db.each("SELECT rowid AS id, info FROM lorem");
 });
 
 module.exports = db
