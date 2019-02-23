275bba5f9d0e30db28e7a29302360a5e0ad07d06
HackerRank <hackerrank@example.com>
2019-02-23 21:10:21 +0000

    Saved at Sat Feb 23 2019 22:10:21 GMT+0100 (West Africa Standard Time)

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var eraseEvents = require('./routes/eraseEvents');
 var events = require('./routes/events');
 var actor = require('./routes/actor');
 
-var app = express(); 
+var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
