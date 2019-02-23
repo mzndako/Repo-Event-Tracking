37939cf1c2dddc75fca4bb0305166b221e97e757
HackerRank <hackerrank@example.com>
2019-02-23 21:49:00 +0000

    Saved at Sat Feb 23 2019 22:48:59 GMT+0100 (West Africa Standard Time)

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const sqlite = require('sqlite3').verbose()
-const db = new sqlite.Database('')
+const db = new sqlite.Database('mz')
 
 db.serialize(function() {
   db.run("CREATE TABLE lorem (info TEXT)");
  
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
   for (var i = 0; i < 10; i++) {
       stmt.run("Ipsum " + i);
   }
