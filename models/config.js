a408003b3995bd33856fccbffadea8758ed75629
HackerRank <hackerrank@example.com>
2019-02-24 10:37:54 +0000

    Saved at Sun Feb 24 2019 11:37:53 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -34,7 +34,7 @@ db.find = async (query, whereBind=[])=>{
 db.insert = async (query, values) => {
   var statement = db.prepare(query);
   statement.run(values)
-  const x = await statement.finalize(a=>console.log("aaa", a))
+  statement.finalize(a=>console.log("aaa", a))
   console.log("xxx," , x)
 }
 
