bfa56b7640152f4105d9c86297a672917c3c1e57
HackerRank <hackerrank@example.com>
2019-02-24 12:11:30 +0000

    Saved at Sun Feb 24 2019 13:11:22 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -44,7 +44,7 @@ const addEvent = async (data) => {
       await database.insert(sql, bindParam);
     }
     
-    return Promise.
+    return Promise.resolve()
   }catch(error){
     return Promise.reject(error);
   }
