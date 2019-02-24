29b86215eb030719ce36d585e8a7967a40432b33
HackerRank <hackerrank@example.com>
2019-02-24 14:52:34 +0000

    Saved at Sun Feb 24 2019 15:52:34 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -70,7 +70,7 @@ const getByActor = (actorId) => {
  * @param {Integer} actorId - Actor Id
  */
 const eraseEvents = () => {
-  let sql = 'DELETE FROM ';
+  let sql = 'DELETE FROM e';
   
   return database.find(sql, [actorId]).then(events => {
     let results = processEvents(events);
