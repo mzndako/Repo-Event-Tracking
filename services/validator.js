3b262b87559df647d1a6a5bb0ff6f5fca99eb3d5
HackerRank <hackerrank@example.com>
2019-02-24 11:38:48 +0000

    Saved at Sun Feb 24 2019 12:38:44 GMT+0100 (West Africa Standard Time)

--- services/validator.js
+++ services/validator.js
@@ -4,7 +4,7 @@
  const validate = (body, expectedError) => {
    for(let key in expectedError){
      if (!expectedError.hasOwnProperty(key)) {
-       continue; // Dont process inhired
+       continue; // Dont process inherited
      }
      let key = body[i];
      if (expectedError[key] && body) {
