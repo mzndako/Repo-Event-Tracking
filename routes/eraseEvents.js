de929f544b3c696d811391f25e8c52cbe9f5d2af
HackerRank <hackerrank@example.com>
2019-02-24 14:48:29 +0000

    Saved at Sun Feb 24 2019 15:48:29 GMT+0100 (West Africa Standard Time)

--- routes/eraseEvents.js
+++ routes/eraseEvents.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const eventsController = require('./../controllers/events')
 // Route related to delete events
-router.delete('/', eventsController.eraseE)
+router.delete('/', eventsController.eraseEvents)
 
 module.exports = router;
