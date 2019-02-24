2e05a8ff455d99a374c9e4c242a5a1800322e727
HackerRank <hackerrank@example.com>
2019-02-24 13:21:31 +0000

    Saved at Sun Feb 24 2019 14:21:31 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -30,6 +30,28 @@ db.find = (query, whereBind=[])=>{
   }) 
 }
 
+/**
+ * Insert into the database
+ * @param {String} query - The insert query String
+ * @param {Array} array - The binded values to be inserted into the database
+ * @return A promise
+ */
+db.insert = async (query, values) => {
+  let statement = db.prepare(query);
+  statement.run(values) // Bind the values
+  return statement.finalize()
+}
+/**
+ * Insert into the database
+ * @param {String} query - The insert query String
+ * @param {Array} array - The binded values to be inserted into the database
+ * @return A promise
+ */
+db.insert = async (query, values) => {
+  let statement = db.prepare(query);
+  statement.run(values) // Bind the values
+  return statement.finalize()
+}
 
  
 db.setupDatabase()
