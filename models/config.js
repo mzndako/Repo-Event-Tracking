67d1aef6e179b9d8e48f30d21af430a098e7e4be
HackerRank <hackerrank@example.com>
2019-02-24 11:04:40 +0000

    Saved at Sun Feb 24 2019 12:04:39 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -15,7 +15,7 @@ db.setupDatabase = () => {
  */
 db.find = async (query, whereBind=[])=>{
   console.log(query)
-  return db.all(query, whereBind, (err, result) => {
+  db.all(query, whereBind, (err, result) => {
     console.log(result, err)
     if(err){
       return Promise.reject(err)
