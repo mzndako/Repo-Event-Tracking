5aee1d25cdfa87e08b6406bbfefe09b21725ddc2
HackerRank <hackerrank@example.com>
2019-02-24 12:05:12 +0000

    Saved at Sun Feb 24 2019 13:03:55 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -22,7 +22,7 @@ const addEvent = async (data) => {
     bindParam = [data.id, data.type, data.actor.id, data.repo.id, new Date()];
     await database.insert(sql, bindParam);
     
-    //
+    // I
     
   }catch(error){
     return Promise.reject(error);
