91f326164f0acf66da67fbc5cee4aa7abda6e359
HackerRank <hackerrank@example.com>
2019-02-24 10:41:00 +0000

    Saved at Sun Feb 24 2019 11:41:00 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -1,7 +1,7 @@
 /**
  * Get all events
  * @param {Object} req - The request Object
- * @param {Object} res - The Response Ob
+ * @param {Object} res - The Response Object
  */
 const getAllEvents = (req, res) => {
     
