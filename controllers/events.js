1adb55ba1b1081ddf70aa9f76893501d99509800
HackerRank <hackerrank@example.com>
2019-02-24 14:51:19 +0000

    Saved at Sun Feb 24 2019 15:51:19 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -68,7 +68,7 @@ const getByActor = (req, res) => {
  */
 const eraseEvents = () => {
   eventsServices.eraseEvents().then(result => {
-    res.status(200).send(processError('Successful'))
+    res.status(200).send)
   }).catch(error => res.status(400).send(processError(error)))
 };
 
