3427adf835aa16c75307afb519ebba28f9bddc43
HackerRank <hackerrank@example.com>
2019-02-24 00:07:17 +0000

    Saved at Sun Feb 24 2019 01:07:15 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -2,14 +2,8 @@ const sqlite = require('sqlite3').verbose()
 const db = new sqlite.Database('database/db.sqlite')
 
 const find = (tableName, whereCommand)=>{
-  let whereBind = []
-  let sql = `SELECT * FROM ${tableName}`
-  if(whereCommand && whereCommand[0]){
-    sql += ` where ${whereCommand[0]}`
-    whereBind = whereCommand.slice(1)
-  }
   
-  return db.all(sql, whereComma, (err, result) => {
+  return db.all(sql, whereCommand, (err, result) => {
     console.log(result, err)
     if(err){
       return Promise.reject(err)
