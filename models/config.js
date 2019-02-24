be36449c9f7a5686e52622e0150c9b74c41f3135
HackerRank <hackerrank@example.com>
2019-02-24 00:19:17 +0000

    Saved at Sun Feb 24 2019 01:18:48 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -4,7 +4,9 @@ const db = new sqlite.Database('database/db.sqlite')
 const setupDatabase = () => {
   try{
     db.run('CREATE TABLE users(ID integer NOT NULL PRIMARY KEY, name text, city text)')
-  }catch
+  }catch(error){
+    console.log(error)
+  }
 }
 
 const find = (query, whereBind=[])=>{
