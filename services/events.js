3cbd7452817636dd801c3c519d44fb7e2a7bbdca
HackerRank <hackerrank@example.com>
2019-02-24 14:44:02 +0000

    Saved at Sun Feb 24 2019 15:44:01 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -4,7 +4,7 @@ const getAllEvents = () => {
   let sql = 'SELECT events.id as event_id, * FROM events INNER JOIN repos ON events.repo_id = repos.id INNER JOIN actors ON events.actor_id = actors.id';
 
   return database.find(sql).then(events => {
-    let results = processeEvents(events);
+    let results = processEvents(events);
     return Promise.resolve(results);
   }).catch(error => Promise.reject(error));
 };
@@ -57,7 +57,7 @@ const addEvent = async (data) => {
  * @param {Integer} actorId - Actor Id
  */
 const getByActor = (actorId) => {
-  let sql = 'SELECT events.id as event_id, * FROM events INNER JOIN repos ON events.repo_id = repos.id INNER JOIN actors ON events.actor_id = actors.id WHERE actor_id = ?';
+  let sql = 'SELECT events.id as event_id, * FROM events INNER JOIN repos ON events.repo_id = repos.id INNER JOIN actors ON events.actor_id = actors.id WHERE actor_id = ? ORDER BY';
   
   return database.find(sql, [actorId]).then(events => {
     let results = processEvents(events);
