0f815919b19f1260d2084fa8fa64000eda1c2d83
HackerRank <hackerrank@example.com>
2019-02-24 12:07:36 +0000

    Saved at Sun Feb 24 2019 13:07:34 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -27,7 +27,7 @@ const addEvent = async (data) => {
     bindParam = [data.actor.id];
     results = database.find(sql, bindParam);
     if (results.length === 0){
-      // Create a new user sin
+      // Create a new user sinc
     }
     sql = 'INSERT INTO actors (id, login, avatar_url) VALUES (?, ?, ?)';
     
