31f7468bb1825933837f0023e029eaf48514df63
HackerRank <hackerrank@example.com>
2019-02-24 13:22:38 +0000

    Saved at Sun Feb 24 2019 14:22:37 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -17,7 +17,6 @@ db.setupDatabase = () => {
  * @return The query result as a promise
  */
 db.find = (query, whereBind=[])=>{
-
   return new Promise((resolve, reject) => {
     db.all(query, whereBind, (err, result) => {
       console.log(result);
@@ -59,7 +58,7 @@ db.setupDatabase()
 
 //db.find('SELECT * FROM events ')
 //db.insert("UPDATE actors SET login = ? WHERE id=?", ['mz', 3213163])
-db.find('SELECT * FROM actors where id = 3213163')
+/db.find('SELECT * FROM actors where id = 3213163')
 //db.find('SELECT * FROM repos ')
 
 module.exports = db
