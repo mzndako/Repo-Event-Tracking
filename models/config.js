3c2649682ee7483db627e8bf821991ac259846a8
HackerRank <hackerrank@example.com>
2019-02-24 13:21:38 +0000

    Saved at Sun Feb 24 2019 14:21:38 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -48,7 +48,7 @@ db.insert = async (query, values) => {
  * @param {Array} array - The binded values to be inserted into the database
  * @return A promise
  */
-db. = async (query, values) => {
+db.update = async (query, values) => {
   let statement = db.prepare(query);
   statement.run(values) // Bind the values
   return statement.finalize()
