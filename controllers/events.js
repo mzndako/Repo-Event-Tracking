c5d1a0f13220b854594c8e8fc1b4d73bf298ac68
HackerRank <hackerrank@example.com>
2019-02-24 13:37:09 +0000

    Saved at Sun Feb 24 2019 14:37:09 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -45,7 +45,7 @@ const getByActor = (req, res) => {
     return res.status(400).send(processError('Invalid Actor Id'));
   }
   
-  eventsServices.getByActor(actorId).then()
+  eventsServices.getByActor(actorId).then(e)
 };
 
 
