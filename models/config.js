7f6c9ce160e1e557059394e3e7fe4384b4b7d9cc
HackerRank <hackerrank@example.com>
2019-02-24 13:19:21 +0000

    Saved at Sun Feb 24 2019 14:19:14 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -46,7 +46,7 @@ db.insert = async (query, values) => {
 db.setupDatabase()
 
 //db.find('SELECT * FROM events ')
-
+db.update("UPDATE actors SET ")
 db.find('SELECT * FROM actors ')
 //db.find('SELECT * FROM repos ')
 
