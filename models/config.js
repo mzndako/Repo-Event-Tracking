62390d2c4f1c5fe052db89d9fc4378a5fdc531f7
HackerRank <hackerrank@example.com>
2019-02-24 11:09:41 +0000

    Saved at Sun Feb 24 2019 12:09:40 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -17,7 +17,7 @@ db.find = (query, whereBind=[])=>{
   console.log(query)
   return new Promise((resolve, reject) => {
     db.all(query, whereBind, (err, result) => {
-    console.log(result, err)
+      console.log(result, err)
     if(err){
       reject(err)
     }
