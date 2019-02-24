2c814163f93f97a08c350a28b4abbd91ca6a0762
HackerRank <hackerrank@example.com>
2019-02-24 12:32:56 +0000

    Saved at Sun Feb 24 2019 13:32:44 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -5,7 +5,7 @@ const getAllEvents = () => {
   
   return database.find(sql).then(results => {
     let processedResults = [];
-    for(let i )
+    for(let i = 0; i < )
     return Promise.resolve(results);
   }).catch(error => Promise.reject(error));
 };
