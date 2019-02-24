47b45f22b286216266e8ecc493027f434fc7fef0
HackerRank <hackerrank@example.com>
2019-02-24 10:37:15 +0000

    Saved at Sun Feb 24 2019 11:37:15 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -28,7 +28,7 @@ db.find = async (query, whereBind=[])=>{
 /**
  * Insert into the database
  * @param {String} query - The insert query String
- * @param {Array} array - The value to 
+ * @param {Array} array - The value to be inserted 
  */
 db.insert = async (query, values) => {
   var statement = db.prepare(query);
