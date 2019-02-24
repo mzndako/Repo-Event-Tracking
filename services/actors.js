35dcbc6dbb2480638dc9ceeb85e503669402363c
HackerRank <hackerrank@example.com>
2019-02-24 15:51:06 +0000

    Saved at Sun Feb 24 2019 16:51:03 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -4,7 +4,7 @@ const database = require('./../models/config');
  * Retrieve all actors
  */
 const getAllActors = () => {
-  let sql = 'SELECT * from actors left join ';
+  let sql = 'SELECT actors.*, count(events.id) as event_count from actors inner join events on actors.id = events.actor_id GROUP BY events.actor_id order by event_count DESC'
 
   return database.find(sql).then(events => {
     let results = processEvents(events);
