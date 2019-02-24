687a8b388dd27341bcdf0185f838beb7a087be92
HackerRank <hackerrank@example.com>
2019-02-24 10:59:01 +0000

    Saved at Sun Feb 24 2019 11:59:01 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -8,7 +8,7 @@ const eventsServices = require('./../services/events');
  */
 const getAllEvents = (req, res) => {
   eventsServices.getAllEvents().then(results => {
-    res.status(201).send
+    res.status(201).send()
   })
 };
 
