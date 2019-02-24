e04a42fee8f6de43e6405a3d12a78726ae483f2a
HackerRank <hackerrank@example.com>
2019-02-24 10:58:33 +0000

    Saved at Sun Feb 24 2019 11:58:33 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -7,7 +7,7 @@ const eventsServices = require('./../services/events');
  * @return null
  */
 const getAllEvents = (req, res) => {
-  eventsServices.getAllEvents().the
+  eventsServices.getAllEvents().then()
 };
 
 var addEvent = () => {
