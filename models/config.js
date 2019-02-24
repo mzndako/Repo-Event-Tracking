7b6336a513a50061d74ef0f6b9b635a0ca0af822
HackerRank <hackerrank@example.com>
2019-02-24 01:20:26 +0000

    Saved at Sun Feb 24 2019 02:20:25 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -3,7 +3,7 @@ const db = new sqlite.Database('database/db.sqlite')
 
 const setupDatabase = () => {
     db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), author_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))')
-    //db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
+    //db.run('CREATE TABLE IF NOT EXISTS event(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
     //db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
 
 }
