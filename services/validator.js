7ef8f3e4c1f481554b7bf696f94681a004e363b6
HackerRank <hackerrank@example.com>
2019-02-24 11:36:36 +0000

    Saved at Sun Feb 24 2019 12:36:32 GMT+0100 (West Africa Standard Time)

--- services/validator.js
+++ services/validator.js
@@ -3,7 +3,7 @@
  */
  const validate = (body, expectedError) => {
    for(let key in expectedError){
-     if(Object.
+     if (Object.
      let key = body[i];
      if (expectedError[key] && body) {
        
