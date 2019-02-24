51e6d2087ebb9e392eba5d20282185e28eecb6bf
HackerRank <hackerrank@example.com>
2019-02-24 13:38:56 +0000

    Saved at Sun Feb 24 2019 14:38:56 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -46,7 +46,7 @@ const getByActor = (req, res) => {
   }
   
   eventsServices.getByActor(actorId).then(events => {
-
+    if(re)
     res.status(200).send(events);
   }).catch(error => res.status(404).send(processError(error)))
 };
