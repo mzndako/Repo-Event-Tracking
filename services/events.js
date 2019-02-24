6077a8c577328d6e68deb0b39fd5bba880e50256
HackerRank <hackerrank@example.com>
2019-02-24 12:27:36 +0000

    Saved at Sun Feb 24 2019 13:27:31 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -29,7 +29,7 @@ const addEvent = async (data) => {
     results = database.find(sql, bindParam);
     if (results.length === 0) {
       // Create a new actor
-      console.log('inserting in')
+      console.log('inserting into autho')
       sql = 'INSERT INTO actors (id, login, avatar_url) VALUES (?, ?, ?)';
       bindParam = [data.actor.id, data.actor.login, data.actor.avatar_url];
       await database.insert(sql, bindParam);
