7507c421c06798530433b048f908ed8524e903c8
HackerRank <hackerrank@example.com>
2019-02-24 10:35:12 +0000

    Saved at Sun Feb 24 2019 11:35:11 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -11,7 +11,7 @@ db.setupDatabase = () => {
 /**
  * Query the database
  * @param {String} - SQL String
- * @param {Array} - The array of bind value for
+ * @param {Array} - The array of bind value for the where 
  */
 db.find = async (query, whereBind=[])=>{
   console.log(query)
