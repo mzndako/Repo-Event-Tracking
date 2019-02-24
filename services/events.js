9e45fabc31ed1e5e4b71b8a686da664201c0998c
HackerRank <hackerrank@example.com>
2019-02-24 10:54:30 +0000

    Saved at Sun Feb 24 2019 11:54:29 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -1,7 +1,7 @@
 const database = require('./../models/config');
 
 const getAllEvents = () => {
-  let sql = 'SELECT * FROM events INNER JOIN repo WHERE events.repo_id = repo.id '
+  let sql = 'SELECT * FROM events INNER JOIN repo WHERE events.repo_id = repo.id I'
   database.find()
 };
 
