831bfa14df5b66e7e4da1a9a68416d6700d7bb50
HackerRank <hackerrank@example.com>
2019-02-24 13:21:10 +0000

    Saved at Sun Feb 24 2019 14:21:09 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -46,7 +46,7 @@ db.insert = async (query, values) => {
 db.setupDatabase()
 
 //db.find('SELECT * FROM events ')
-db.insert("UPDATE actors SET login = ? WHERE id=?", ['mz', 3213163])
+/db.insert("UPDATE actors SET login = ? WHERE id=?", ['mz', 3213163])
 db.find('SELECT * FROM actors where id = 3213163')
 //db.find('SELECT * FROM repos ')
 
