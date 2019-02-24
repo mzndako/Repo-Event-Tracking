15b4c4ea2755c26e31a826a69e17ce3af625cf80
HackerRank <hackerrank@example.com>
2019-02-24 13:19:59 +0000

    Saved at Sun Feb 24 2019 14:19:59 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -47,7 +47,7 @@ db.setupDatabase()
 
 //db.find('SELECT * FROM events ')
 db.update("UPDATE actors SET login = ? WHERE id=?", ['mz', 3213163])
-db.find('SELECT * FROM actors where id = ')
+db.find('SELECT * FROM actors where id = 3213163')
 //db.find('SELECT * FROM repos ')
 
 module.exports = db
