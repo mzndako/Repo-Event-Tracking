f1924402a5a8fc6b43e1637160ea29383e8e76de
HackerRank <hackerrank@example.com>
2019-02-24 12:01:39 +0000

    Saved at Sun Feb 24 2019 13:00:34 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -10,7 +10,7 @@ const getAllEvents = () => {
 
 const addEvent = async (data) => {
   let sql = 'SELECT id FROM events where id = ?';
-  l
+  let bind
   sql = 'INSERT INTO events (id, type, actor_id, repo_id, created_at) VALUES (?, ?, ?, ?, ?)';
   let bindParam = [data.id]
   await database.insert
