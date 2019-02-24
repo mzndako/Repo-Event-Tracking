3fde15d1c6b64a3fb1f4fc99e5e46dc05773d6c4
HackerRank <hackerrank@example.com>
2019-02-24 10:56:37 +0000

    Saved at Sun Feb 24 2019 11:56:36 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -4,7 +4,7 @@ const getAllEvents = () => {
   let sql = 'SELECT * FROM events INNER JOIN repo WHERE events.repo_id = repo.id INNER JOIN author WHERE events.author_id = author.id'
   return database.find(sql).then(results=>{
     return Promise.resolve(results)
-  }).catch(error=>)
+  }).catch(error =)
 };
 
 module.exports = {
