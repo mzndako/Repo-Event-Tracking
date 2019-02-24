db17ed91e7851120e65798cabe2f871c61ddf9bc
HackerRank <hackerrank@example.com>
2019-02-24 01:25:52 +0000

    Saved at Sun Feb 24 2019 02:25:51 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -1,7 +1,7 @@
 const sqlite = require('sqlite3').verbose()
 var db = new sqlite.Database('database/db.sqlite')
 
-db. setupDatabase = () => {
+db.setupDatabase = () => {
     db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), author_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))')
     db.run('CREATE TABLE IF NOT EXISTS author(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
     db.run('CREATE TABLE IF NOT EXISTS repo(id INTEGER, name VARCHAR(200), url VARCHAR(500), PRIMARY KEY(id))')
