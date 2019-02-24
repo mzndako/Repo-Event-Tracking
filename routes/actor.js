6c84064a3b0f5506eaa7846f74481bebed5d2c60
HackerRank <hackerrank@example.com>
2019-02-24 14:59:02 +0000

    Saved at Sun Feb 24 2019 15:59:01 GMT+0100 (West Africa Standard Time)

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const actorsController = require('./../co')
+const actorsController = require('./../controllers/')
 // Routes related to actor.
 router.get('/', )
 module.exports = router;
