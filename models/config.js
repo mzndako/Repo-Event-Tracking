e6c9db01ec4831ea2049ba8ee393fc12f987fedf
HackerRank <hackerrank@example.com>
2019-02-24 01:02:55 +0000

    Saved at Sun Feb 24 2019 02:02:39 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -30,7 +30,7 @@ const insert = (query, values) => {
 setupDatabase()
 
 var x = await insert("INSERT INTO mzee VALUES ( ?, ?)", ['okay1', 'mzndako3'])
-console.log(x, "bbbbb")
+console.log()
 find("select * from mzee")
 
 module.exports = db
