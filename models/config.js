cb2e685f4c9201c8a197c078247be2c8b65d203c
HackerRank <hackerrank@example.com>
2019-02-25 21:06:14 +0000

    Saved at Mon Feb 25 2019 22:06:13 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -71,7 +71,7 @@ db.delete = async (query) => {
 
 db.setupDatabase();
 
-//db.find('SELECT * FROM events ')
+db.find('SELECT * FROM events ')
 //db.insert("UPDATE actors SET login = ? WHERE id=?", ['mz', 3213163])
 //db.find('SELECT * FROM actors where id = 3213163')
 //db.find('SELECT * FROM repos ')
