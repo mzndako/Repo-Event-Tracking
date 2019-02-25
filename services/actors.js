22711f422554b8cdc7e3534ad4c8b38359bc50b3
HackerRank <hackerrank@example.com>
2019-02-25 23:19:34 +0000

    Saved at Tue Feb 26 2019 00:19:34 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -77,7 +77,7 @@ const getStreak = async (actorId) => {
       }
       compare.id = actor.id;
       compare.count++;
-      compare.created_at = events[]
+      compare.created_at = events[i].cr
     }
     console.log(compare);
     return Promise.resolve(actors);
