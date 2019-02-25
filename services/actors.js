823df668119b32c056a576b568bffff1a741db91
HackerRank <hackerrank@example.com>
2019-02-25 21:44:52 +0000

    Saved at Mon Feb 25 2019 22:44:51 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -52,6 +52,26 @@ const getActor = async (actorId) => {
   }
 };
 
+/**
+ * Get an actor detail
+ * @param {String} actorId - Actor unique id
+ * @return A promise of Actor details or null if actor not found
+ */
+const getActor = async (actorId) => {
+  let sql = 'SELECT * FROM actors WHERE id = ?';
+  let bindParam = [actorId];
+
+  try {
+    let result = await database.find(sql, bindParam);
+    if (result.length === 0) {
+       return null; // No Actor not found
+    };
+    return result[0];
+  } catch (error) {
+    return null;
+  }
+};
+
 module.exports = {
   getAllActors,
   updateActor,
