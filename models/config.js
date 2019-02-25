0a628f7e0e5a88521906cb54343cc75e8b5c70e0
HackerRank <hackerrank@example.com>
2019-02-25 20:39:47 +0000

    Saved at Mon Feb 25 2019 21:39:47 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -14,7 +14,7 @@ db.dropAllTables = async () => {
   await db.run('DROP TABLE IF EXISTS events');
   await db.run('DROP TABLE IF EXISTS actors');
   await db.run('DROP TABLE IF EXISTS repos');
-  console.log("done drop")
+  console.log("done droping")
   return Promise.resolve();
 }
 
