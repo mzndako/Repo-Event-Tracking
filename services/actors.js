3199e43a85139eeb853607b4342b492a679b26e4
HackerRank <hackerrank@example.com>
2019-02-25 22:47:23 +0000

    Saved at Mon Feb 25 2019 23:47:23 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -58,7 +58,7 @@ const getActor = async (actorId) => {
  * @return A promise of Actor details or null if actor not found
  */
 const getStreak = async (actorId) => {
-  getAllActors().then
+  getAllActors().then(a)
 };
 
 module.exports = {
