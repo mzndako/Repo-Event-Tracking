296d6303ebcb9028ee3c2205f71d8426b6ae7633
HackerRank <hackerrank@example.com>
2019-02-25 19:57:12 +0000

    Saved at Mon Feb 25 2019 20:57:12 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -21,7 +21,7 @@ const getAllActors = () => {
 const updateActor = (actor) => {
   let {id, login, avatar_url} = actor;
   try{
-    
+    return P
   }catch(error){
     return Promise.reject(error);
   }
