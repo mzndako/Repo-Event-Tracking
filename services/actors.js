effc05286c85a8dc157ce33b51715d61789757ea
HackerRank <hackerrank@example.com>
2019-02-25 23:29:06 +0000

    Saved at Tue Feb 26 2019 00:29:05 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -100,7 +100,7 @@ const getStreak = async (actorId) => {
       return -1;
     });
     sortedActors = sortedActors.map(actor => {
-      
+      delete ac
     })
     return Promise.resolve(sortedActors);
   }).catch(error => console.log(error));
