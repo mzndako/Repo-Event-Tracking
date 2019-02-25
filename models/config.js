6bdbb85114f31cad195714b45a25aa44e3a2d0fe
HackerRank <hackerrank@example.com>
2019-02-25 20:41:44 +0000

    Saved at Mon Feb 25 2019 21:41:44 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -6,9 +6,9 @@ var db = new sqlite.Database('database/db.sqlite')
  */
 db.setupDatabase = async () => {
   db.serialize(() => {
-  await db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), actor_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))')
-  await db.run('CREATE TABLE IF NOT EXISTS actors(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
-  await db.run('CREATE TABLE IF NOT EXISTS repos(id INTEGER, name VARCHAR(200), url VARCHAR(500), PRIMARY KEY(id))')
+    await db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), actor_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))')
+    await db.run('CREATE TABLE IF NOT EXISTS actors(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
+    await db.run('CREATE TABLE IF NOT EXISTS repos(id INTEGER, name VARCHAR(200), url VARCHAR(500), PRIMARY KEY(id))')
   }
   console.log("done creating")
 }
