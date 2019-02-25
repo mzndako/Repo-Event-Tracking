a4f13e793f8740188d0a56454df088f4b569cea2
HackerRank <hackerrank@example.com>
2019-02-25 20:02:40 +0000

    Saved at Mon Feb 25 2019 21:02:39 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -31,7 +31,7 @@ const updateActor = async (actor) => {
     if(dbActor.avatar_url !== avatar_url){
       return Promise.reject(400); // Not allowed to update the avatar_url
     }
-    datab
+    database.update
     return Promise.resolve()
   }catch(error){
     return Promise.reject(error);
