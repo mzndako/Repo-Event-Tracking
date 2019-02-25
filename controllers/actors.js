e95f3e086fa2beb85a3ec22077b6d33feb043278
HackerRank <hackerrank@example.com>
2019-02-25 20:05:06 +0000

    Saved at Mon Feb 25 2019 21:05:06 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -3,12 +3,12 @@ const {processError} = require('./../services/validator');
 
 var getAllActors = (req, res) => {
   actorsServices.getAllActors().then(actors => {
-    res.status(200).send(actors)
+    res.status(200).send(actors);
   }).catch(error => res.status(400).send(processError(error)))
 };
 
 var updateActor = () => {
-
+  
 };
 
 var getStreak = () => {
