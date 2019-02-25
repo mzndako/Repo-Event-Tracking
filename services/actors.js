be396dae56ce6c5d9cbb576bca68cf6bb5bc7001
HackerRank <hackerrank@example.com>
2019-02-25 23:48:03 +0000

    Saved at Tue Feb 26 2019 00:48:03 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -103,7 +103,7 @@ const getStreak = async (actorId) => {
           return -1;  
         }
       }
-      console.log("name Comparison",)
+      console.log("name Comparison", )
       if (a.login < b.login) {
         return 1;
       } else if(a.login > b.login) {
