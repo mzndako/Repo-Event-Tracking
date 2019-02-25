e379a44d66252ceea5e302e1143faa420a6cc50b
HackerRank <hackerrank@example.com>
2019-02-25 20:01:42 +0000

    Saved at Mon Feb 25 2019 21:01:41 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -27,7 +27,7 @@ const updateActor = async (actor) => {
     if(result.length === 0){
       return Promise.reject(404)
     }
-    let retriev
+    let retrieveActo
     return Promise.resolve()
   }catch(error){
     return Promise.reject(error);
