e8595c89661a42847f0f707b2748407753928ca8
HackerRank <hackerrank@example.com>
2019-02-25 21:30:46 +0000

    Saved at Mon Feb 25 2019 22:30:45 GMT+0100 (West Africa Standard Time)

--- test/tests.js
+++ test/tests.js
@@ -87,6 +87,7 @@ describe('git_test ', function() {
 		 					expect(ar2.length).to.equal(ar1.length);
 		 					
 							for (let k = 0; k < ar1.length; k++) {
+
 								expect(ar2[k]).to.deep.equal(ar1[k]);
 							}
 						}
