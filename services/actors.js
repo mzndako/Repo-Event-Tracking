b1e91f50ec2f274077834e4a104ce0a219641ecd
HackerRank <hackerrank@example.com>
2019-02-25 22:47:09 +0000

    Saved at Mon Feb 25 2019 23:47:09 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -57,7 +57,7 @@ const getActor = async (actorId) => {
  * @param {String} actorId - Actor unique id
  * @return A promise of Actor details or null if actor not found
  */
-const getStre = async (actorId) => {
+const getStreak = async (actorId) => {
   let sql = 'SELECT * FROM actors WHERE id = ?';
   let bindParam = [actorId];
 
@@ -75,5 +75,5 @@ const getStre = async (actorId) => {
 module.exports = {
   getAllActors,
   updateActor,
-  getActor
+  getActor,
 }
