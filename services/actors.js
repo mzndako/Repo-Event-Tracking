9b41693774747ed01fd535d5e13bf8047dc33a6b
HackerRank <hackerrank@example.com>
2019-02-25 20:04:33 +0000

    Saved at Mon Feb 25 2019 21:04:33 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -17,7 +17,7 @@ const getAllActors = () => {
 
 /**
  * Update actor
- * @param {Object} actor - Actor object conti
+ * @param {Object} actor - Actor object containing the
  */
 const updateActor = async (actor) => {
   let {id, login, avatar_url} = actor;
