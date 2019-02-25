1fddddb181b3fb08e70ad529bf5ec3269c109316
HackerRank <hackerrank@example.com>
2019-02-25 21:44:58 +0000

    Saved at Mon Feb 25 2019 22:44:58 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -53,7 +53,7 @@ const getActor = async (actorId) => {
 };
 
 /**
- * Get an actor 
+ * Get an actor streak
  * @param {String} actorId - Actor unique id
  * @return A promise of Actor details or null if actor not found
  */
