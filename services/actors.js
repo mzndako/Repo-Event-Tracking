2beb1b1d97923c0174c3b3a5974b057431ea5443
HackerRank <hackerrank@example.com>
2019-02-25 20:15:56 +0000

    Saved at Mon Feb 25 2019 21:15:56 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -23,7 +23,7 @@ const updateActor = async (actor) => {
   let {id, avatar_url} = actor;
   try{
     let sql = 'UPDATE actors SET login = ? WHERE id = ?';
-    let bindParam = [, id];
+    let bindParam = [avatar_url, id];
     await database.update(sql, bindParam);
 
     return Promise.resolve('Updated');
