30f931594c71f00ca30266e01f1c74fdaa228a9e
HackerRank <hackerrank@example.com>
2019-02-25 23:45:21 +0000

    Saved at Tue Feb 26 2019 00:45:21 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -101,7 +101,7 @@ const getStreak = async (actorId) => {
         }
       }
       if()
-      return -1;
+      return ;
     });
     // sortedActors = sortedActors.map(actor => {
     //   return {
