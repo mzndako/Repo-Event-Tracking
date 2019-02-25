69d60536793b639c57ff790c62286ff252dce745
HackerRank <hackerrank@example.com>
2019-02-25 20:14:38 +0000

    Saved at Mon Feb 25 2019 21:14:37 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -13,7 +13,7 @@ var updateActor = (req, res) => {
     if(!retrievedActor){
       return res.status(404).send(processError('Actor not found'))
     }
-    if(actor.avatar_url !== retrievedActor.login){
+    if(actor.avatar_ur !== retrievedActor.login){
       return res.status(404).send(processError('You can only update avatar_url'))
     }
   })
