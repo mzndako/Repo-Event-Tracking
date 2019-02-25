dbd0a8a128055354a8e231e32e6b9f9d078c49eb
HackerRank <hackerrank@example.com>
2019-02-25 23:01:14 +0000

    Saved at Tue Feb 26 2019 00:01:14 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -66,7 +66,9 @@ const getStreak = async (actorId) => {
       if(!actors[actor.id]){
         actors[actor.id] = actor;
       }
-      if(compare.id !== actor.id)
+      if(compare.id !== actor.id){
+        
+      }
     }
   });
 };
