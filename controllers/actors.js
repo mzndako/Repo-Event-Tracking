991e3edea45d102f30b7f818eb29b492e1800192
HackerRank <hackerrank@example.com>
2019-02-25 20:29:34 +0000

    Saved at Mon Feb 25 2019 21:29:34 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -23,6 +23,7 @@ const updateActor = async (req, res) => {
     let updated = await actorsServices.updateActor(actor);
     res.status(200).send(updated);
   } catch (error) {
+
     res.status(400).send(processError(error));
   }
 };
