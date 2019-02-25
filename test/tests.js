babc26b24050803316298d5a28a1ece0c3362e90
HackerRank <hackerrank@example.com>
2019-02-25 21:43:07 +0000

    Saved at Mon Feb 25 2019 22:43:07 GMT+0100 (West Africa Standard Time)

--- test/tests.js
+++ test/tests.js
@@ -83,11 +83,10 @@ describe('git_test ', function() {
 							if(e.response.status_code == 404) {
 								continue;
 							}
-							console.log(ar2.length, "===", ar1.length)
+							
 		 					expect(ar2.length).to.equal(ar1.length);
 		 					
 							for (let k = 0; k < ar1.length; k++) {
-							  console.log(ar2[k].id)
 								expect(ar2[k]).to.deep.equal(ar1[k]);
 							}
 						}
