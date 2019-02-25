1b59d1383fb94f82ca1bb122724bbcd822c8de06
HackerRank <hackerrank@example.com>
2019-02-25 20:46:42 +0000

    Saved at Mon Feb 25 2019 21:46:42 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -6,7 +6,7 @@ var db = new sqlite.Database('database/db.sqlite')
  */
 db.setupDatabase = async (dropTable) => {
   db.serialize(() => {
-    if ()
+    if (d) {
       db.run('DROP TABLE IF EXISTS events');
       db.run('DROP TABLE IF EXISTS actors');
       db.run('DROP TABLE IF EXISTS repos');
