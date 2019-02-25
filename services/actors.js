f46b1046c829a20106882eef672a865bed3007c3
HackerRank <hackerrank@example.com>
2019-02-25 22:55:59 +0000

    Saved at Mon Feb 25 2019 23:55:58 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -60,7 +60,7 @@ const getActor = async (actorId) => {
 const getStreak = async (actorId) => {
   getAllActors().then(events => {
     let newEvents = {};
-    let compare = {lastId: "", lastCoun};
+    let compare = {lastId: "", l};
     for (let i = 0; i < events.length; i++) {
       
     }
