b67a321d563662186bb47da46f3e1f6b1391a894
HackerRank <hackerrank@example.com>
2019-02-25 20:43:46 +0000

    Saved at Mon Feb 25 2019 21:43:45 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -9,7 +9,7 @@ db.setupDatabase = async () => {
     db.run('DROP TABLE IF EXISTS events');
     db.run('DROP TABLE IF EXISTS actors');
     db.run('DROP TABLE IF EXISTS repos');
-    
+  
     db.serialize(() => {
       db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), actor_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))');
       db.run('CREATE TABLE IF NOT EXISTS actors(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))');
