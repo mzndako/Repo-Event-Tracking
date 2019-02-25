e649d0e74461043fb70152c43803e7722fb48aa5
HackerRank <hackerrank@example.com>
2019-02-25 19:59:29 +0000

    Saved at Mon Feb 25 2019 20:59:28 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -24,7 +24,7 @@ const updateActor = (actor) => {
     let sql = 'SELECT * FROM actors WHERE id = ?';
     let bindParam = [id];
     let result = database.find(sql, bindParam);
-    if(res)
+    if(result.)
     return Promise.resolve()
   }catch(error){
     return Promise.reject(error);
