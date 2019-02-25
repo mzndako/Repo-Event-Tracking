8d36909504c91a1b5efb4faa6dc38660f6627230
HackerRank <hackerrank@example.com>
2019-02-25 20:03:20 +0000

    Saved at Mon Feb 25 2019 21:03:19 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -35,7 +35,7 @@ const updateActor = async (actor) => {
     }
     
     sql = 'UPDATE actors SET login = ? WHERE id = ?';
-    
+    bindParam
     database.update()
     return Promise.resolve()
   }catch(error){
