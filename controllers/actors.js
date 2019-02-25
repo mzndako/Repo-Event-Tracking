081f64780b1413652dc4debb735df87ecb9bd500
HackerRank <hackerrank@example.com>
2019-02-25 20:12:14 +0000

    Saved at Mon Feb 25 2019 21:12:13 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -9,7 +9,7 @@ var getAllActors = (req, res) => {
 
 var updateActor = (req, res) => {
   let actor = req.body.actor;
-  actorsServices.getActor(actor.id).
+  actorsServices.getActor(actor.id).then 
 };
 
 var getStreak = () => {
