77175917e8562c8cecf75d002a7cd4f0ef6a0fa1
HackerRank <hackerrank@example.com>
2019-02-25 20:33:52 +0000

    Saved at Mon Feb 25 2019 21:33:51 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -4,7 +4,7 @@ var db = new sqlite.Database('database/db.sqlite')
 /**
  * Call the function to setup the database
  */
-db.setupDatabase = asy() => {
+db.setupDatabase = async () => {
   await db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), actor_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))')
   await db.run('CREATE TABLE IF NOT EXISTS actors(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
   await db.run('CREATE TABLE IF NOT EXISTS repos(id INTEGER, name VARCHAR(200), url VARCHAR(500), PRIMARY KEY(id))')
