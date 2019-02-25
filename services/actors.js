90649143b7814bd30e62685d9bfc60c5167ecc37
HackerRank <hackerrank@example.com>
2019-02-25 23:26:40 +0000

    Saved at Tue Feb 26 2019 00:26:39 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -92,7 +92,7 @@ const getStreak = async (actorId) => {
         return 1;
       }
       if(a.count === b.count){
-        if(new Date)
+        if(new Date())
       }
       return false;
     });
