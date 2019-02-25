24835071a02915a7a8844b19a0a8672afb10f559
HackerRank <hackerrank@example.com>
2019-02-25 19:58:55 +0000

    Saved at Mon Feb 25 2019 20:58:55 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -21,7 +21,7 @@ const getAllActors = () => {
 const updateActor = (actor) => {
   let {id, login, avatar_url} = actor;
   try{
-    let sql = 'SELECT * FROM actors WHERE '
+    let sql = 'SELECT * FROM actors WHERE id '
     let result = database.find()
     return Promise.resolve()
   }catch(error){
