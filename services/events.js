1b3362b2d938dfe797815149a78d74f328a59ffc
HackerRank <hackerrank@example.com>
2019-02-25 21:21:25 +0000

    Saved at Mon Feb 25 2019 22:21:25 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -4,7 +4,7 @@ const database = require('./../models/config');
  * Retrieve all events
  */
 const getAllEvents = () => {
-  let sql = 'SELECT events.id as event_id, * FROM events INNER JOIN repos ON events.repo_id = repos.id INNER JOIN actors ON events.actor_id = actors.id ORDER B';
+  let sql = 'SELECT events.id as event_id, * FROM events INNER JOIN repos ON events.repo_id = repos.id INNER JOIN actors ON events.actor_id = actors.id ORDER BY i';
 
   return database.find(sql).then(events => {
     let results = processEvents(events);
