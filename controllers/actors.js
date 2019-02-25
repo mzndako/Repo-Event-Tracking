85edffdfe0b1c6f5a457378d90b4dfdf3ec502aa
HackerRank <hackerrank@example.com>
2019-02-25 20:17:18 +0000

    Saved at Mon Feb 25 2019 21:17:17 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -9,6 +9,7 @@ var getAllActors = (req, res) => {
 
 var updateActor = async (req, res) => {
   let actor = req.body.actor;
+
   actorsServices.getActor(actor.id).then(retrievedActor => {
     if(!retrievedActor){
       return res.status(404).send(processError('Actor not found'))
