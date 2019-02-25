8ac5ef316e40c17b2ba07118d9ce935077596f52
HackerRank <hackerrank@example.com>
2019-02-25 20:42:41 +0000

    Saved at Mon Feb 25 2019 21:42:41 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -5,7 +5,7 @@ var db = new sqlite.Database('database/db.sqlite')
  * Call the function to setup the database
  */
 db.setupDatabase = async () => {
-  db.serialize()
+  db.serialize( ())
   db.serialize(() => {
     db.run('CREATE TABLE IF NOT EXISTS events(id INTEGER, type VARCHAR(50), actor_id INTEGER, repo_id INTEGER, created_at DATETIME, PRIMARY KEY(id))')
     db.run('CREATE TABLE IF NOT EXISTS actors(id INTEGER, login VARCHAR(50), avatar_url VARCHAR(500), PRIMARY KEY(id))')
