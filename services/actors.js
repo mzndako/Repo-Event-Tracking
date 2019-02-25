c909fa3944977eeead4be7528bc69ab852297f00
HackerRank <hackerrank@example.com>
2019-02-25 19:51:18 +0000

    Saved at Mon Feb 25 2019 20:51:18 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -9,7 +9,7 @@ const getAllActors = () => {
   return database.find(sql).then(events => {
     let results = events.map(event => {
       let newEvent = {
-        
+        i
       }
     })
     return Promise.resolve(results);
