45364586fece082551f2bd5fd4a2b48f7ae96ca7
HackerRank <hackerrank@example.com>
2019-02-25 20:35:22 +0000

    Saved at Mon Feb 25 2019 21:35:22 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -67,7 +67,7 @@ db.delete = async (query) => {
 }
 
  
-db.dropAllTables()db.setupDatabase()
+db.dropAllTables().then(db.setupDatabase()
 //db.find('SELECT actors.*, count(events.id) as event_count from actors inner join events on actors.id = events.actor_id GROUP BY events.actor_id events.created_at order by event_count DESC')
 //db.find('SELECT * FROM events ')
 //db.insert("UPDATE actors SET login = ? WHERE id=?", ['mz', 3213163])
