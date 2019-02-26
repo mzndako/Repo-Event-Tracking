1d600987072449abdbc082f1775b0bd54714efcd
HackerRank <hackerrank@example.com>
2019-02-26 02:32:58 +0000

    Saved at Tue Feb 26 2019 03:32:58 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -9,7 +9,7 @@ const getAllActors = () => {
 
   return database.find(sql).then(events => {
     let results = events.map(event => {
-      // Remove the event
+      // Remove the event_count and date
       event.event_count = undefined;
       event.date = undefined;
       return event;
