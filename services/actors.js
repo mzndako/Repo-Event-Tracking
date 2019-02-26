3c089eae26dddc183b110d31695393b74e131b25
HackerRank <hackerrank@example.com>
2019-02-26 00:07:57 +0000

    Saved at Tue Feb 26 2019 01:07:57 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -5,7 +5,7 @@ const eventsServices = require('./events');
  * Retrieve all actors
  */
 const getAllActors = () => {
-  let sql = 'SELECT actors.*, count(events.id) as event_count, max from actors inner join events on actors.id = events.actor_id GROUP BY events.actor_id order by event_count DESC'
+  let sql = 'SELECT actors.*, count(events.id) as event_count, max() from actors inner join events on actors.id = events.actor_id GROUP BY events.actor_id order by event_count DESC'
 
   return database.find(sql).then(events => {
     let results = events.map(event => {
