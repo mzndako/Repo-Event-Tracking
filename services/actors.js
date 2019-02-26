7835546823a16bed683b8a2558ff5c0e253669d8
HackerRank <hackerrank@example.com>
2019-02-26 03:27:58 +0000

    Saved at Tue Feb 26 2019 04:27:58 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -105,7 +105,7 @@ const getStreak = async (actorId) => {
         return -1;
       }
       
-      // Sort if the 
+      // Sort if the Strea
       if(a.count === b.count){
         let date1 = new Date(a.created_at).getTime();
         let date2 = new Date(b.created_at).getTime();
