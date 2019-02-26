24c88270aa0a9c33f1eacbcb6183ed17ab488c66
HackerRank <hackerrank@example.com>
2019-02-26 00:31:37 +0000

    Saved at Tue Feb 26 2019 01:31:36 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -19,7 +19,7 @@ const getAllEvents = (req, res) => {
  * @param {Object} res - The Response Object
  * @return Response Resource
  */
-const addEvent = asy(req, res) => {
+const addEvent = async (req, res) => {
   let data = req.body;
   let validationRule = {
     'id': 'Please provide a valid Id',
