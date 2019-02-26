020c4c4d3f7edb287e26f41520a275fa52360e73
HackerRank <hackerrank@example.com>
2019-02-26 03:34:48 +0000

    Saved at Tue Feb 26 2019 04:34:48 GMT+0100 (West Africa Standard Time)

--- services/validator.js
+++ services/validator.js
@@ -2,7 +2,7 @@
  * Valid a request
  * @param {Object} body - Object you want to validate
  * @param {Object} expectedError - The error message to return for each of the object validator set
- * @return The filtered
+ * @return 
  */
 const validate = (body, expectedError) => {
  for (let key in expectedError) {
