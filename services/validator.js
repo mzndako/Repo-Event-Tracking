1710f66e03019886338538aeaf34c9165dd88a65
HackerRank <hackerrank@example.com>
2019-02-26 03:31:46 +0000

    Saved at Tue Feb 26 2019 04:31:46 GMT+0100 (West Africa Standard Time)

--- services/validator.js
+++ services/validator.js
@@ -15,7 +15,7 @@ const validate = (body, expectedError) => {
 
 /**
  * Convert and format the error 
- * @param {Object} 
+ * @param {Object} error - 
  */
 const processError = (error) => {
  let sendError = {};
