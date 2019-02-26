197134a35f2cef5fa47dfa9f67ef1bcc084f380c
HackerRank <hackerrank@example.com>
2019-02-26 02:26:07 +0000

    Saved at Tue Feb 26 2019 03:26:07 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -76,7 +76,7 @@ const getByActor = (actorId) => {
  * Erase all events, actors and repos
  */
 const eraseEvents = async () => {
-
+  try{
   let sql = 'DELETE FROM events';
   await database.delete(sql);
   await database.delete(sql);
