3b89871c9ea5b704d1de713bac8e11fd8ab8996d
HackerRank <hackerrank@example.com>
2019-02-26 01:36:55 +0000

    Saved at Tue Feb 26 2019 02:36:55 GMT+0100 (West Africa Standard Time)

--- test/tests.js
+++ test/tests.js
@@ -83,11 +83,8 @@ describe('git_test ', function() {
 							if(e.response.status_code == 404) {
 								continue;
 							}
-							
-							console.log("==",e.request.url);
 		 					expect(ar2.length).to.equal(ar1.length);
 							for (let k = 0; k < ar1.length; k++) {
-							  console.log(ar1[k],)
 								expect(ar2[k]).to.deep.equal(ar1[k]);
 							}
 						}
