3a6d45bb27c4bb4229720bca151eb5606c3ab9af
HackerRank <hackerrank@example.com>
2019-02-26 02:35:12 +0000

    Saved at Tue Feb 26 2019 03:35:12 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -67,7 +67,7 @@ const getStreak = async (actorId) => {
     for (let i = 0; i < events.length; i++) {
       let actor = events[i].actor;
 
-      // Check if the actor has already been inserted
+      // Check if the actor has already been inserted into the 
       if(!actors[actor.id]){
         actors[actor.id] = actor;
         actors[actor.id].count = 1;
