296f43c65a963a6be708620d175e570c039b24cc
HackerRank <hackerrank@example.com>
2019-02-26 02:31:55 +0000

    Saved at Tue Feb 26 2019 03:31:54 GMT+0100 (West Africa Standard Time)

--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('./../controllers/events');
 // Routes related to event
-
 router.get('/', eventController.getAllEvents);
 router.post('/', eventController.addEvent);
 router.get('/actors/:actorId', eventController.getByActor);
