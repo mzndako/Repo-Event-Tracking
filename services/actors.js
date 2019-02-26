1db8cf0bc777e67c401bb2955445f244b73b038c
HackerRank <hackerrank@example.com>
2019-02-26 03:27:11 +0000

    Saved at Tue Feb 26 2019 04:27:11 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -92,7 +92,7 @@ const getStreak = async (actorId) => {
     let newActors = [];
     for(let key in actors){
       if(!actors.hasOwnProperty(key)){
-        continue; 
+        continue; //Ke
       }
       newActors.push(actors[key]);
     }
