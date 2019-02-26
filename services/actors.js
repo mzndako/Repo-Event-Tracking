fb70643e8ae3f938f6a58b42e9ffa5ed6d5c24cb
HackerRank <hackerrank@example.com>
2019-02-26 03:29:50 +0000

    Saved at Tue Feb 26 2019 04:29:50 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -140,7 +140,7 @@ const getStreak = async (actorId) => {
 /**
  * Calculate the day differences between two date
  * @param {String} date1 - The first date (from date)
- * @param 
+ * @param {Srin
  */
 const date_diff_indays = (date1, date2) => {
   let dt1 = new Date(date1);
