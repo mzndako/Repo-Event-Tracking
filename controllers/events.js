9ee1edfef6e1b97f2fc9098fa37b125c48550ff6
HackerRank <hackerrank@example.com>
2019-02-26 00:32:44 +0000

    Saved at Tue Feb 26 2019 01:32:44 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -21,7 +21,7 @@ const getAllEvents = (req, res) => {
  */
 const addEvent = async (req, res) => {
   let data = req.body;
-  al.map(data => {
+  data.map(data => {
     eventsServices.addEvent(data)
   })
   let validationRule = {
