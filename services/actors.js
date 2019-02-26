7c7cebc9d5f54c4e4de5cd3a80cc4165ed8e69d6
HackerRank <hackerrank@example.com>
2019-02-26 03:29:04 +0000

    Saved at Tue Feb 26 2019 04:29:04 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -137,7 +137,8 @@ const getStreak = async (actorId) => {
   }).catch(error => Promise.reject(error));
 };
 
-/*
+/**
+ * 
 const date_diff_indays = (date1, date2) => {
   let dt1 = new Date(date1);
   let dt2 = new Date(date2);
