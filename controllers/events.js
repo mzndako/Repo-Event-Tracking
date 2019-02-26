6fab2340a3d15777ec773c4dd204f20d089d4e78
HackerRank <hackerrank@example.com>
2019-02-26 00:33:13 +0000

    Saved at Tue Feb 26 2019 01:33:12 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -22,6 +22,7 @@ const getAllEvents = (req, res) => {
 const addEvent = async (req, res) => {
   let data = req.body;
   data.map(d => {
+
     await eventsServices.addEvent(d);
   })
   return res.send();
