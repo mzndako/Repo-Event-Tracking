888bf7858397532d81d63b5076ece8de8ce157ca
HackerRank <hackerrank@example.com>
2019-02-26 02:09:20 +0000

    Saved at Tue Feb 26 2019 03:09:20 GMT+0100 (West Africa Standard Time)

--- services/actors.js
+++ services/actors.js
@@ -131,7 +131,7 @@ const getStreak = async (actorId) => {
 };
 
 const date_diff_indays = (date1, date2) => {
-  ldt1 = new Date(date1);
+  let dt1 = new Date(date1);
   dt2 = new Date(date2);
   return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
 }
