eff633368694b2a8b013998959b5688f29d4e990
HackerRank <hackerrank@example.com>
2019-02-26 00:32:03 +0000

    Saved at Tue Feb 26 2019 01:32:03 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -21,7 +21,7 @@ const getAllEvents = (req, res) => {
  */
 const addEvent = async (req, res) => {
   let all = req.body;
-  all
+  all.map
   let validationRule = {
     'id': 'Please provide a valid Id',
     'type': 'Please provide a type',
