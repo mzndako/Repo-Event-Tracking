91e7eb6349ec868b8ed1a97a5e8d67aeeedc2cfb
HackerRank <hackerrank@example.com>
2019-02-26 01:33:51 +0000

    Saved at Tue Feb 26 2019 02:33:51 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -42,7 +42,7 @@ const updateActor = async (req, res) => {
 };
 
 /**
- * Get  Streak by actor
+ * Get Actor Streak by actor
  * @param {Object} req - The Request Object
  * @param {Object} res - The Response Object
  * @return null
