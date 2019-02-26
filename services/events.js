d84fd8307dbca3ca5e205c040f41b0ae1cd8e84e
HackerRank <hackerrank@example.com>
2019-02-26 02:25:31 +0000

    Saved at Tue Feb 26 2019 03:25:31 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -77,7 +77,7 @@ const getByActor = (actorId) => {
  */
 const eraseEvents = async () => {
   let sql = 'DELETE FROM events';
-  await database.delete(sql).then(result => {
+  let statawait database.delete(sql).then(result => {
     return Promise.resolve(result);
   }).catch(error => Promise.reject(error));
 }
