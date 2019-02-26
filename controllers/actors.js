4ba60f13bcb7e875e11ed60426960bbc2805c694
HackerRank <hackerrank@example.com>
2019-02-26 01:33:07 +0000

    Saved at Tue Feb 26 2019 02:33:07 GMT+0100 (West Africa Standard Time)

--- controllers/actors.js
+++ controllers/actors.js
@@ -14,7 +14,7 @@ const getAllActors = (req, res) => {
 };
 
 /**
- * Get events by actor
+ * 
  * @param {Object} req - The Request Object
  * @param {Object} res - The Response Object
  * @return null
