92361a41a483f54d692c6533bed7f1325074d9a1
HackerRank <hackerrank@example.com>
2019-02-26 02:30:27 +0000

    Saved at Tue Feb 26 2019 03:30:26 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -40,7 +40,7 @@ db.find = (query, whereBind = []) => {
 };
 
 /**
- * Insert into a table
+ * QInsert into a table
  * @param {String} query - The insert query String
  * @param {Array} array - The binded values to be inserted into the table
  * @return A promise
