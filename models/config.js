10338a09cb26612091264c7ff63ef759edb5e9be
HackerRank <hackerrank@example.com>
2019-02-26 02:30:00 +0000

    Saved at Tue Feb 26 2019 03:30:00 GMT+0100 (West Africa Standard Time)

--- models/config.js
+++ models/config.js
@@ -2,7 +2,7 @@ const sqlite = require('sqlite3').verbose();
 var db = new sqlite.Database('database/db1.sqlite');
 
 /**
- * Call the function to setup the database by creating the necessary t
+ * Call the function to setup the database by creating the necessary tables
  */
 db.setupDatabase = async (dropTable) => {
   db.serialize(() => {
