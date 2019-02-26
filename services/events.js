13e05ff1d8bb6cd933d9b2bc982c8028eed4dff1
HackerRank <hackerrank@example.com>
2019-02-26 02:26:57 +0000

    Saved at Tue Feb 26 2019 03:26:56 GMT+0100 (West Africa Standard Time)

--- services/events.js
+++ services/events.js
@@ -80,7 +80,7 @@ const eraseEvents = async () => {
     let sql = 'DELETE FROM events';
     await database.delete(sql);
     
-    sql =
+    sql = 'DELETE'
     await database.delete(sql);
     return Promise.resolve('Deleted');
   } catch (error) {
